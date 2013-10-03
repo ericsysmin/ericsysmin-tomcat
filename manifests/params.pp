340c15536444950485c244a015929daf71522bb9
Eric Anderson <erican@angieslist.com>
2013-10-02 19:55:43 -0700

    updated to fit better form

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class tomcat::params {
-  $version   = "${tomcat::version}"
-  $java_ops  = "${tomcat::java_ops}"
-  $java_home = "${tomcat::java_home}"
+  $version   = $tomcat::version
+  $java_ops  = $tomcat::java_ops
+  $java_home = $tomcat::java_home
 }
